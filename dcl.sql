91d37bd1e1be4b5491156481ce61b2f57a385a97
KANGMINSEO0 <juhee0629032@gmail.com>
2024-05-03 20:04:53 +0900

    PROJECT3 DB

---
+++ dcl.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE BEANIONSDB;
+
+GRANT ALL PRIVILEGES ON beanionsdb.* TO 'ohgiraffers'@'%';
+
+-- REVOKE ALL PRIVILEGES ON testdb.* FROM 'ohgiraffers'@'%';
+
+SHOW GRANTS FOR 'ohgiraffers'@'%';
+
+USE BEANIONSDB;
